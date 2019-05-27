1f78bfe6398ade4de808dd6803eeb73c0c3f216c
maria <mariaaquil09@gmail.com>
2019-05-27 13:32:48 +0530

    Task 2 completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,12 +12,16 @@ public class Main {
         ctx.refresh();
 
         Movie bean = (Movie) ctx.getBean("Movie.class");
-        System.out.println("movie id: "+bean.getId());
-        System.out.println("movie name: " +bean.getMovieName());
-        System.out.println("actor's name: "+ bean.getActor().getName());
-        System.out.println("actor's age: "+bean.getActor().getAge());
-        System.out.println("actor's gender: "+bean.getActor().getGender());
+//        System.out.println("movie id: "+bean.getId());
+//        System.out.println("movie name: " +bean.getMovieName());
+//        System.out.println("actor's name: "+ bean.getActor().getName());
+//        System.out.println("actor's age: "+bean.getActor().getAge());
+//        System.out.println("actor's gender: "+bean.getActor().getGender());
+        System.out.println(bean.toString());
 
+        Movie movie1=(Movie) ctx.getBean("Movie.class");
+        Movie movie2=(Movie) ctx.getBean("Movie.class");
+        System.out.println(movie1==movie2);
     }
 
 }
--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -5,20 +5,24 @@ import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 
 public class SpringBeanConfiguration {
     @Bean(name="Movie.class")
+    @Scope("prototype")
     public Movie getMovie(){
-        Actor actor=new Actor();
-        actor.setName("Shahid Kapoor");
-        actor.setGender("Male");
-        actor.setAge(38);
-        Movie movie=new Movie();
-        movie.setId(15);
-        movie.setMovieName("Kabir Singh");
-        movie.setActor(actor);
+//        Actor actor=new Actor();
+//        actor.setName("Shahid Kapoor");
+//        actor.setGender("Male");
+//        actor.setAge(38);
+//        Movie movie=new Movie();
+//        movie.setId(15);
+//        movie.setMovieName("Kabir Singh");
+//        movie.setActor(actor);
+        Actor actor = new Actor("Katrina", "Female", 33);
+        Movie movie = new Movie(20, "Bharat", actor);
         return movie;
     }
 
