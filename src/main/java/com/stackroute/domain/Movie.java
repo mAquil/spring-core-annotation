6d5a6c9f50203f0b09bd963fd5386f3aeccd0223
maria <mariaaquil09@gmail.com>
2019-05-27 17:57:01 +0530

    Task 4 is completed

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,12 +1,18 @@
 package com.stackroute.domain;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     private int id;
     private String movieName;
-    @Autowired
+//    @Autowired
     private Actor actor;
 
 //    public Movie() {
@@ -37,6 +43,7 @@ public class Movie {
         this.movieName = movieName;
     }
 
+
 //    public Movie(int id, String movieName, Actor actor) {
 //        this.id = id;
 //        this.movieName = movieName;
@@ -58,6 +65,25 @@ public class Movie {
                 ", movieName='" + movieName + '\'' +
                 ", actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("hey "+beanFactory);
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("bean name is: "+s );
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            System.out.println(applicationContext);
+            System.out.println(applicationContext.getBean("Actor.class"));
+
     }
 }
 
